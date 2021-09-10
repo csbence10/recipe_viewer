c58037f0b7f9c1b593a87b2237de5a037a9b1af3
Bence Csondor <csbence10@gmail.com>
2021-09-10 19:24:21 +0200

    Add base project and dependencies

---
+++ lib/app/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_viewer/app/recipe_viewer_app.dart';
+
+void main() {
+  runApp(RecipeViewerApp());
+}
---
+++ lib/app/recipe_viewer_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecipeViewerApp extends StatelessWidget {
+  const RecipeViewerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
